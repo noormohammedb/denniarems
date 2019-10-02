4c13a9eba4fa9877e10107787a6f1f92475d2227
Dennis Sam <denniarems@gmail.com>
2019-10-02 23:35:05 +0530

    first commit

---
+++ precache-manifest.45b1c5cf5c57028071e182ab63f49bea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b068f740fc0aa589df30",
+    "url": "/js/chunk-vendors.b00b8ee5.js"
+  },
+  {
+    "revision": "cffd5a18b73d12c1e010",
+    "url": "/js/app.8878af7d.js"
+  },
+  {
+    "revision": "53507ef485499592e1fcd235d82a4a55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81bb4521ab8a664a4bb54b1a23e71f23",
+    "url": "/img/Dennis.81bb4521.png"
+  },
+  {
+    "revision": "b068f740fc0aa589df30",
+    "url": "/css/chunk-vendors.6b01f858.css"
+  },
+  {
+    "revision": "cffd5a18b73d12c1e010",
+    "url": "/css/app.1c647734.css"
+  }
+];
